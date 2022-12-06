[fix] modify masking method

explainer/input_reduction_ig_approx.py
@@ -1,3 +1,4 @@
+import re
 import pathlib
 import itertools
 import argparse
@@ -13,7 +14,7 @@ import torch
 
 from models.model import BertClassifier
 
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers_interpret import SequenceClassificationExplainer
 
 from tqdm import tqdm
@@ -26,62 +27,79 @@ logging.basicConfig(format='[%(asctime)s] [%(levelname)s] <%(funcName)s> %(messa
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+MODEL_NAME = 'cardiffnlp/twitter-roberta-base-sentiment'
+
 
 def load_data(data_dir, IF):
     if IF:
         dataset = load_from_disk(data_dir / 'reduced_train')
     else:
-        dataset = load_dataset('glue', 'sst2', split='validation')
+        dataset = load_dataset('tweet_eval', 'sentiment', split='test')
         dataset = dataset.shuffle(seed=42).select(range(500))
     return dataset
 
-def load_model(model_path, device="cuda"):
-    logger.info(f"Loading model from {model_path}")
+def load_model(model_path, device='cuda'):
+    logger.info(f'Loading model from {model_path}')
     model = BertClassifier.from_pretrained(model_path, num_labels=2)
     model = model.to(device)
     return model
 
-def create_erased_inputs(x, s, cls_explainer, tokenizer):
-    word_attributions = cls_explainer(s)
+def clean_text(text):
+        text = re.sub('([.,!?()])', r' \1 ', text)
+        text = re.sub('\s{2,}', ' ', text)
+        return text
 
-    erased = list()
-    mod_input_ids = x.input_ids[0].clone()
+def create_erased_inputs(encoded, sentence, cls_explainer, tokenizer):
+    word_attributions = cls_explainer(sentence)
 
-    word_attributions = [(w, sc) for w, sc in word_attributions if w != ""]
+    erased = []
+    mod_input_ids = encoded.input_ids[0].clone()
 
-    if len(word_attributions) != mod_input_ids.shape[0]:
-        return None
+    word_attributions = [(word, score) for word, score in word_attributions if word != '']
+    
+    # format input_ids to the same format as transformers_interpret
+    for i, token in enumerate(tokenizer.convert_ids_to_tokens(mod_input_ids)):
+        if token == 'Ġ':
+            # delete the ith token
+            mod_input_ids = torch.cat([mod_input_ids[:i], mod_input_ids[i+1:]])
+
+    assert len(word_attributions) == len(mod_input_ids), f'{word_attributions} != {tokenizer.convert_ids_to_tokens(mod_input_ids)}'
 
     for i, _ in sorted(enumerate(word_attributions), key=lambda x: x[1][1]):
-        if i == 0 or i == x.input_ids[0].shape[0]-1:
+        if i == 0 or i == encoded.input_ids[0].shape[0]-1:
             continue
-
         mod_input_ids[i] = tokenizer.mask_token_id
+        
         erased.append(mod_input_ids.clone())
+    
+    # if there is only one token in sentence, return None
+    if len(erased) == 1:
+        return None
     
     return torch.stack(erased[:-1])
 
-def enumerate_hypothesis(s, cls_explainer, model, tokenizer):
-    x = tokenizer(s, return_tensors="pt").to("cuda")
+def enumerate_hypothesis(sentence, cls_explainer, model, tokenizer):
+    sentence = clean_text(sentence)
+    encoded = tokenizer(sentence, return_tensors='pt').to('cuda')
 
-    with torch.no_grad():
-        output = model(input_ids=x['input_ids'])
+    with torch.inference_mode():
+        output = model(input_ids=encoded['input_ids'])
         origin_logits = output.logits[0].cpu().numpy()
 
-        erased_inputs = create_erased_inputs(x, s, cls_explainer, tokenizer)
+        erased_inputs = create_erased_inputs(encoded, sentence, cls_explainer, tokenizer)
 
         if erased_inputs is None:
             return None
 
         output = model(input_ids=erased_inputs)
 
-    predictions = list()
+    predictions = []
 
-    for logit, masked_x, s in zip(output.logits, erased_inputs, tokenizer.batch_decode(erased_inputs)):
+    for logit, masked_sentence, masked_sentence_seq in zip(output.logits, erased_inputs, tokenizer.batch_decode(erased_inputs)):
         logit = logit.cpu().softmax(dim=0).numpy()
-
-        predictions.append(MaskedPattern(masked_sentence=s,
-                                         masked_len= len([t for t in masked_x if t == tokenizer.mask_token_id]),
+        
+        predictions.append(MaskedPattern(masked_sentence=masked_sentence_seq,
+                                         masked_len= len([token for token in masked_sentence if token == tokenizer.mask_token_id]),
                                          correct=origin_logits.argmax() == logit.argmax(),
                                          score_diff=origin_logits.max() - logit.max(),
                                          origin_pred=origin_logits.argmax().item(),
@@ -92,8 +110,10 @@ def enumerate_hypothesis(s, cls_explainer, model, tokenizer):
     return predictions
 
 def main(args):
-    model = load_model(args.model_path, device=args.device)
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+    # model = load_model(args.model_path, device=args.device)
+    model = model.to(args.device)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
     cls_explainer = SequenceClassificationExplainer(
         model,
@@ -114,19 +134,26 @@ def main(args):
                 masked = enumerate_hypothesis(s, cls_explainer, model, tokenizer)
                 masked_inf.append(masked)
     else:
-        logger.info("Iterating reductive maksing...")
-        masked_pattern = [enumerate_hypothesis(data['sentence'], cls_explainer, model, tokenizer)[0] for data in tqdm(pattern_data)]
+        logger.info('Iterating reductive maksing...')
+        masked_pattern = []
+        none = 0
+        for data in tqdm(pattern_data):
+            pattern = enumerate_hypothesis(data['text'], cls_explainer, model, tokenizer)
+            if pattern is not None:
+                masked_pattern.append(pattern[0])
+            else:
+                none += 1
+        logger.info(f'Including None: {none}')
         
-        logger.info(f"Saving to {args.output_dir}")
+        logger.info(f'Saving to {args.output_dir}')
         with open(args.output_dir / 'masked_pattern.pkl', 'wb') as f:
             pickle.dump(masked_pattern, f)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--IF", action="store_true")
-    parser.add_argument("--device", type=str, default="cuda")
-    parser.add_argument("--task", type=str, default='SA')
-    parser.add_argument("--model_name", type=str, default='bert-base-uncased')
+    parser.add_argument('--IF', action='store_true')
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--task', type=str, default='SA')
     args = parser.parse_args()
 
     BASE_DIR = pathlib.Path(__file__).parent.parent