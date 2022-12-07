a56b5897d4d734b1a604bf68e3f283698bca2ba2
Daichi.H-kagayaki <dash400air@gmail.com>
2022-12-07 16:06:52 +0900

    [update] add examples predictions

--- explainer/data_utils.py
+++ explainer/data_utils.py
@@ -7,12 +7,14 @@ class MaskedPattern:
     masked_len: int
     correct: bool
     score_diff: float
-    origin_pred: float
-    masked_pred: float
+    origin_pred: int
+    masked_pred: int
 
 
 @dataclass
 class Examples(MaskedPattern):
     keywords: list
     examples_idx: list
+    example_preds: list = None
     generality: float = None
+    
