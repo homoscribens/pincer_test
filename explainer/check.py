ad6b65d38f10a563ad13392a1121695a07e9c8e3
Daichi.H-kagayaki <dash400air@gmail.com>
2022-12-14 12:12:37 +0900

    [update] add some functions for visualization

--- explainer/check.py
+++ explainer/check.py
@@ -2,6 +2,7 @@ import pickle
 import pathlib
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from .data_utils import Examples, MaskedPattern
@@ -43,25 +44,102 @@ def prediction_dist(fig_dir, examples):
     ax.bar(np.arange(0, 3), np.bincount(preds))
     plt.savefig(fig_dir / 'prediction_dist.png')
     
+def example_prediction_dist(fig_dir, examples):
+    preds = [e.example_preds for e in examples if e.example_preds is not None]
+    preds = [p for ps in preds for p in ps]
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, xlabel='Prediction', ylabel='Count', title='Example prediction distribution')
+    ax.bar(np.arange(0, 3), np.bincount(preds))
+    plt.savefig(fig_dir / 'example_prediction_dist.png')
+    
 def generality_vs_num_examples(fig_dir, examples):
-    generality = []
-    num_examples = []
+    gen_pos, gen_neu, gen_neg = [], [], []
+    num_pos, num_neu, num_neg = [], [], []
     for e in examples:
         if e.generality is not None:
-            generality.append(e.generality)
-            num_examples.append(len(e.examples_idx))
+            if e.origin_pred == 2:
+                gen_pos.append(e.generality)
+                num_pos.append(len(e.examples_idx))
+            elif e.origin_pred == 1:    
+                gen_neu.append(e.generality)
+                num_neu.append(len(e.examples_idx))
+            else:
+                gen_neg.append(e.generality)
+                num_neg.append(len(e.examples_idx))
     
     fig = plt.figure()
     ax = fig.add_subplot(111, yscale='log', xlabel='Generality', ylabel='Number of examples', title='Generality vs Number of examples')
-    ax.scatter(generality, num_examples)
+    ax.scatter(gen_pos, num_pos)
+    ax.scatter(gen_neu, num_neu)
+    ax.scatter(gen_neg, num_neg)
+    fig.legend(['Positive', 'Neutral', 'Negative'])
     fig.savefig(fig_dir / 'generality_vs_num_examples.png')
+    
+def generality_vs_example_f1(fig_dir, examples):
+    generality = []
+    f1 = []
+    for e in examples:
+        if e.generality is not None:
+            generality.append(e.generality)
+            f1.append(e.example_f1)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, xlabel='Generality', ylabel='F1', title='Generality vs Example F1')
+    ax.scatter(generality, f1)
+    fig.savefig(fig_dir / 'generality_vs_example_f1.png')
+    
+def create_dataframes(examples):
+    df_list = []
+    for e in examples:
+        if e.generality is not None:
+            preds_dist = np.bincount(e.example_preds)
+            if len(preds_dist) == 2:
+                preds_dist = np.append(preds_dist, 0)
+            elif len(preds_dist) == 1:
+                preds_dist = np.append(preds_dist, [0, 0])
+            else:
+                pass
+            
+            df_list.append({'keywords': [kw.replace('Ġ', '') for kw in e.keywords],
+                            'pattern': e.masked_sentence,
+                            'label': e.origin_pred,
+                            'gold_label': e.gold_label,
+                            'correct': True if e.origin_pred == e.gold_label else False,
+                            'generality': e.generality, 
+                            'num_examples': len(e.examples_idx),
+                            'truncate': e.isTruncated,
+                            'example_f1': e.example_f1,
+                            'pred_0 %': preds_dist[0] / len(e.example_preds),
+                            'pred_1 %': preds_dist[1] / len(e.example_preds),
+                            'pred_2 %': preds_dist[2] / len(e.example_preds)})
+            
+    df = pd.DataFrame(df_list)
+    return df
+
+def glance_at_data(examples):
+    for e in examples:
+        if e.generality is not None:
+            if e.generality >=0.7 and len(e.examples_idx) > 100:
+                print([kw.replace('Ġ', '') for kw in e.keywords])
+                print(e.generality) 
+                print(len(e.examples_idx))
+                print(e.origin_pred)
+                print('-----------------')
 
 def main(examples,fig_dir):
+    
+    # glance_at_data(examples)
     generality_hist(fig_dir, examples)
     generality_vs_prediction(fig_dir, examples)
     num_examples_hist(fig_dir, examples)
     prediction_dist(fig_dir, examples)
+    example_prediction_dist(fig_dir, examples)
     generality_vs_num_examples(fig_dir, examples)
+    generality_vs_example_f1(fig_dir, examples)
+    
+    df = create_dataframes(examples)
+    df.to_csv(fig_dir / 'data.csv')
     
 if __name__ == '__main__':
     BASE = BASE_DIR = pathlib.Path(__file__).parent.parent
